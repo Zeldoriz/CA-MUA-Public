39f20cef37e3d51479ca393764441cf803a8f889
Andrew Tian <exparados@gmail.com>
2025-03-18 20:36:03 +0800

    config changes

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 3000,
-    host: '0.0.0.0'
-  },
   server: {
-    port: 3000,
-    host: '0.0.0.0'
+    port: 8000,
+    host: true,
+    watch: {
+      usePolling: true
+    }
   }
 });
